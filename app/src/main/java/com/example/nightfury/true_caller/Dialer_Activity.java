f45c1dc5b243c0d24f44db60b1a48a90b444f6a9
Fahim Rahman <mefahimrahman@gmail.com>
2017-04-15 00:13:49 +0600

    Dialer Layout Activity Added... Its Time to Connect Database & Search Layout...

--- app/src/main/java/com/example/nightfury/true_caller/Dialer_Activity.java
+++ app/src/main/java/com/example/nightfury/true_caller/Dialer_Activity.java
@@ -1,123 +1,205 @@
 package com.example.nightfury.true_caller;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.TextView;
 
-public class Dialer_Activity extends AppCompatActivity implements View.OnClickListener {
+public class Dialer_Activity extends AppCompatActivity {
 
-            private EditText screen;
+    private static final int MY_PERMISSIONS_REQUEST =1;
+
+    TextView tv_number;
+    Button b_1,b_2,b_3,b_4,b_5,b_6,b_7,b_8,b_9,b_0,b_star,b_hash,b_call,b_clear,b_bclear;
+    String number = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dialer_layout);
-        initializeView();
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.CALL_PHONE)){
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, MY_PERMISSIONS_REQUEST);
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, MY_PERMISSIONS_REQUEST);
+            }
+        }
+
+        tv_number = (TextView) findViewById(R.id.tv_number);
+        b_1 = (Button) findViewById(R.id.b_1);
+        b_2 = (Button) findViewById(R.id.b_2);
+        b_3 = (Button) findViewById(R.id.b_3);
+        b_4 = (Button) findViewById(R.id.b_4);
+        b_5 = (Button) findViewById(R.id.b_5);
+        b_6 = (Button) findViewById(R.id.b_6);
+        b_7 = (Button) findViewById(R.id.b_7);
+        b_8 = (Button) findViewById(R.id.b_8);
+        b_9 = (Button) findViewById(R.id.b_9);
+        b_0 = (Button) findViewById(R.id.b_0);
+        b_star = (Button) findViewById(R.id.b_star);
+        b_hash = (Button) findViewById(R.id.b_hash);
+        b_call = (Button) findViewById(R.id.b_call);
+        b_clear = (Button) findViewById(R.id.b_clear);
+        b_bclear = (Button) findViewById(R.id.bclear);
+
+
+        b_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                number = number + "1";
+                tv_number.setText(number);
+            }
+        });
+
+        b_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                number = number + "2";
+                tv_number.setText(number);
+            }
+        });
+
+        b_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                number = number + "3";
+                tv_number.setText(number);
+            }
+        });
+
+        b_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                number = number + "4";
+                tv_number.setText(number);
+            }
+        });
+
+        b_5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                number = number + "5";
+                tv_number.setText(number);
+            }
+        });
+
+        b_6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                number = number + "6";
+                tv_number.setText(number);
+            }
+        });
+
+        b_7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                number = number + "7";
+                tv_number.setText(number);
+            }
+        });
+
+        b_8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                number = number + "8";
+                tv_number.setText(number);
+            }
+        });
+
+        b_9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                number = number + "9";
+                tv_number.setText(number);
+            }
+        });
+
+        b_0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                number = number + "0";
+                tv_number.setText(number);
+            }
+        });
+
+        b_star.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                number = number + "*";
+                tv_number.setText(number);
+            }
+        });
+
+        b_hash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                number = number + "#";
+                tv_number.setText(number);
+            }
+        });
+
+        b_call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_CALL);
+                i.setData(Uri.parse("tel:"+number));
+                startActivity(i);
+            }
+        });
+
+        b_clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                number = "";
+                tv_number.setText(number);
+            }
+        });
+        b_bclear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(tv_number.getText().toString().length() >= 1) {
+                    number = tv_number.getText().toString().substring(0,tv_number.getText().toString().length()-1);
+                    tv_number.setText(number);
+                }
+
+            }
+        });
+
+
     }
 
-    public void showDialer(View view){
-        Intent i = new Intent(this,Dialer_Activity.class);
+
+    public void showDialar(View view) {
+        Intent i = new Intent(this, Dialer_Activity.class);
         startActivity(i);
-
     }
 
-    public void showSearch(View view){
+    public void showContacts(View v) {
+        Intent i = new Intent(this, Contacts_Activity.class);
+        startActivity(i);
+    }
+
+    public void showBlocks(View v) {
+        Intent i = new Intent(this, Block_Activity.class);
+        startActivity(i);
+    }
+
+    public void showSearch(View v) {
         Intent i = new Intent(this, Search_Activity.class);
         startActivity(i);
     }
-
-    public void showBlock(View view){
-        Intent i = new Intent(this,Block_Activity.class);
-        startActivity(i);
-    }
-
-    public void showContacts(View view){
-        Intent i = new Intent(this,Contacts_Activity.class);
-        startActivity(i);
-    }
-
-    private void initializeView() {
-
-        screen = (EditText) findViewById(R.id.edittext);
-        int idList[] = {R.id.b1, R.id.b2, R.id.b3,
-                R.id.b4, R.id.b5, R.id.b6,
-                R.id.b7, R.id.b8, R.id.b9,
-                R.id.bstar, R.id.b0, R.id.bhash,
-                R.id.dial1, R.id.dial2, R.id.del};
-
-        for (int i : idList) {
-            View v = (View) findViewById(i);
-            v.setOnClickListener(this);
-        }
-    }
-
-    public void display(String val){
-        screen.append(val);
-    }
-
-    private boolean checkCallPermission(){
-        String permission =  "android.permission.CALL_PHONE";
-        int res = getApplicationContext().checkCallingOrSelfPermission(permission);
-        return (res == PackageManager.PERMISSION_GRANTED);
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.b1:
-                display("1");
-                break;
-            case R.id.b2:
-                display("2");
-                break;
-            case R.id.b3:
-                display("3");
-                break;
-            case R.id.b4:
-                display("4");
-                break;
-            case R.id.b5:
-                display("5");
-                break;
-            case R.id.b6:
-                display("6");
-                break;
-            case R.id.b7:
-                display("7");
-                break;
-            case R.id.b8:
-                display("8");
-                break;
-            case R.id.b9:
-                display("9");
-                break;
-            case R.id.b0:
-                display("0");
-                break;
-            case R.id.bstar:
-                display("*");
-                break;
-            case R.id.bhash:
-                display("#");
-                break;
-            case R.id.dial1:
-                if(screen.getText().toString().isEmpty())
-                    Toast.makeText(getApplicationContext(),"First Enter Digits Then Click",Toast.LENGTH_SHORT).show();
-                else if(checkCallPermission())
-                    startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:"+screen.getText())));
-                break;
-
-            case R.id.del:
-                if(screen.getText().toString().length()>=1) {
-                    String newScreen = screen.getText().toString().substring(0,screen.getText().toString().length()-1);
-                    screen.setText(newScreen);
-                }
-                break;
-        }
-    }
 }
+
+    
+
+
